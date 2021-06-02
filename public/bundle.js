28d58232d2a44464077b9c34a21a3433bb4d49c0
Michael Cruz <mrcruz1989@gmail.com>
2021-06-02 15:11:25 -0400

    babel config edits

--- public/bundle.js
+++ public/bundle.js
@@ -100,10 +100,10 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _config_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./config.js */ "./config.js");
 
 
-axios__WEBPACK_IMPORTED_MODULE_0___default.a.defaults.headers.common['Authorization'] = "Token ".concat(_config_js__WEBPACK_IMPORTED_MODULE_1__["API_KEY"]); // for all requests
+axios__WEBPACK_IMPORTED_MODULE_0___default.a.defaults.headers.common['Authorization'] = `Token ${_config_js__WEBPACK_IMPORTED_MODULE_1__["API_KEY"]}`; // for all requests
 
 var requests = {
-  pullWorkouts: "https://wger.de/api/v2/exerciseinfo/?limit=500"
+  pullWorkouts: `https://wger.de/api/v2/exerciseinfo/?limit=500`
 };
 /* harmony default export */ __webpack_exports__["default"] = (requests);
 
